4c73b09c4f3999833583f94684c6b1ca1892714a
bretkoceri <betimretkoceri@gmail.com>
2023-02-07 21:54:48 +0100

    functional_app

--- main.py
+++ main.py
@@ -3,18 +3,36 @@
 import pathlib
 from pathlib import Path
 
-p = Path(__file__)
-data_path = str(p.cwd())+'\data\\'
+# p = Path(__file__)
+# data_path = str(p.cwd())+'\data\\'
 
-file_input = data_path+'data_input.csv'
+# file_input = data_path+'data_input.csv'
 
-try:
-    data = open(file_input)
+
+
+
+
+
+import pandas as pd
+import streamlit as st
+
+file_input = st.file_uploader('Upload your txt file ')
+
+
+
+
+# data = pd.read_csv(file_input)
+# try:
+#     data = open(file_input)
     
-except  OSError:
-    print("File not found!")
+# except  OSError:
+#     print("File not found!")
     
 
+spectra = st.file_uploader("upload file", type={"csv", "txt"})
+if spectra is not None:
+    spectra_df = pd.read_csv(spectra)
+    st.write(spectra_df)
 
 data_list = data.readlines()
 data_range = len(data_list)
--- read_pandas.py
+++ read_pandas.py
@@ -1,19 +1,85 @@
 import pandas as pd
 
-from pathlib import Path
+# from pathlib import Path
 
-p = Path(__file__)
-data_path = str(p.cwd())+'\data\\'
+# p = Path(__file__)
+# data_path = str(p.cwd())+'\data\\'
 
-file_input = data_path+'data_input.csv'
+# file_input = data_path+'data_input.csv'
 
-df = pd.read_csv(file_input)
+import streamlit as st
 
-print(type(df))
-print(type(df['email']))
-df['domain'] = df['email'].apply(lambda x : x.split('@')[1])
-print(df['domain'].value_counts(normalize=True).plot())
-print(df['domain'].info())
+file_input = st.file_uploader('Please upload your file', type=['csv','txt'])
+
+# df= None
+
+if file_input:
+    df = pd.read_csv(file_input)
+    df['domain'] = df['email'].apply(lambda x : x.split('@')[1])
+
+
+if st.button('Print columns'):
+    st.write(df['domain'])
+
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+if st.button('Generate file '):
+    csv = convert_df(df['domain'])
+    st.download_button(
+        label="Download data as CSV",   
+        data=csv,
+        file_name='domains.csv',
+        mime='text/csv',  
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # d = {'a': 1, 'b': 2, 'c': 3}
 # ser = pd.Series(data=d, index=['a', 'b', 'c'])
